2802fbc58ea8ba431ecdc29980de11a2d7923d36
Gabriel Linhares <linhares.gs96@gmail.com>
2024-01-02 16:27:09 -0300

    feat(part3): work with url configs

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -21,4 +21,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_text
+        return f"{self.choice_text} | {self.question.pub_date}"
